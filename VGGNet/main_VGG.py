98be400639be69eb891e201b17464850185eea0c
RudrapratapSinghTomer <rudrawork01@gmail.com>
2026-02-18 07:20:09 +0530

    Commit Working VGGNet

--- VGGNet/main_VGG.py
+++ VGGNet/main_VGG.py
@@ -7,63 +7,68 @@ from torchvision import datasets, transforms
 
 class VGG_(nn.Module):
     def __init__(self):
-        super(VGG_).__init__()
-        conv11 = nn.Conv2d(3, 64, 3, padding=1, stride=1)
-        conv12 = nn.Conv2d(64, 64, 3, padding=1, stride=1)
+        super(VGG_, self).__init__()
+        self.conv11 = nn.Conv2d(3, 64, 3, padding=1, stride=1)
+        self.conv12 = nn.Conv2d(64, 64, 3, padding=1, stride=1)
 
-        conv21 = nn.Conv2d(64, 128, 3, padding=1, stride=1)
-        conv22 = nn.Conv2d(128, 128, 3, padding=1, stride=1)
+        self.conv21 = nn.Conv2d(64, 128, 3, padding=1, stride=1)
+        self.conv22 = nn.Conv2d(128, 128, 3, padding=1, stride=1)
 
-        conv31 = nn.Conv2d(128, 256, 3, padding=1, stride=1)
-        conv32 = nn.Conv2d(256, 256, 3, padding=1, stride=1)
+        self.conv31 = nn.Conv2d(128, 256, 3, padding=1, stride=1)
+        self.conv32 = nn.Conv2d(256, 256, 3, padding=1, stride=1)
 
-        conv41 = nn.Conv2d(256, 512, 3, padding=1, stride=1)
-        conv42 = nn.Conv2d(512, 512, 3, padding=1, stride=1)
+        self.conv41 = nn.Conv2d(256, 512, 3, padding=1, stride=1)
+        self.conv42 = nn.Conv2d(512, 512, 3, padding=1, stride=1)
 
-        conv51 = nn.Conv2d(512, 512, 3, padding=1, stride=1)
-        conv52 = nn.Conv2d(512, 512, 3, padding=1, stride=1)
+        self.conv51 = nn.Conv2d(512, 512, 3, padding=1, stride=1)
+        self.conv52 = nn.Conv2d(512, 512, 3, padding=1, stride=1)
 
-        fc1 = nn.Linear(25088, 4096)
-        fc2 = nn.Linear(4096, 4096)
-        fc3 = nn.Linear(4096, 1000)
+        # self.fc1 = nn.Linear(25088, 4096) 
+        self.fc1 = nn.Linear(512, 4096) #using 32*32 original CIFAR size image to save memory
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 10)
 
-    def forward_pass(self, x):
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
         x = F.relu(self.conv11(x))
         x = F.relu(self.conv12(x))
-        x = nn.MaxPool2d(x, kernel_size=2, stride=2)
+        x = self.maxpool(x)
 
         x = F.relu(self.conv21(x))
         x = F.relu(self.conv22(x))
-        x = nn.MaxPool2d(x, kernel_size=2, stride=2)
+        x = self.maxpool(x)
 
         x = F.relu(self.conv31(x))
         x = F.relu(self.conv32(x))
-        x = nn.MaxPool2d(x, kernel_size=2, stride=2)
+        x = self.maxpool(x)
 
         x = F.relu(self.conv41(x))
         x = F.relu(self.conv42(x))
-        x = nn.MaxPool2d(x, kernel_size=2, stride=2)
+        x = self.maxpool(x)
 
         x = F.relu(self.conv51(x))
         x = F.relu(self.conv52(x))
-        x = nn.MaxPool2d(x, kernel_size=2, stride=2)
+        x = self.maxpool(x)
 
-        x = nn.Flatten(x, 1)
+        x = self.flatten(x)
 
         x = F.relu(self.fc1(x))
-        nn.Dropout(p=0.5)
+        x = F.dropout(x, p=0.5, training=self.training)
 
         x = F.relu(self.fc2(x))
-        nn.Dropout(p=0.5)
+        x = F.dropout(x, p=0.5, training=self.training)
 
-        x = F.relu(self.fc3(x))
-        nn.Dropout(p=0.5)
+        x = self.fc3(x)
 
         return x
 
 transform = transforms.Compose([
+                        #transforms.Resize((224,224)), #using 32*32 original CIFAR size image to save memory
                             transforms.ToTensor(), 
-                                transforms.Normalize((0.5,), (0.5,))])
+                                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
 
 train_dataset = datasets.CIFAR10(root='./data',
                                  train=True,
@@ -75,15 +80,14 @@ test_dataset = datasets.CIFAR10(root='./data',
                                 transform=transform,
                                 download=True)
 
-train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
-test_dataloader = DataLoader(test_dataset, batch_size=256, shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
-device = torch.device(f'cuda' if torch.cuda.is_available else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = VGG_().to(device=device)
-model.to(device)
-optimizer = optim.SGD(model.get_parameter, lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9, weight_decay=0.0005)
 loss_fn = nn.CrossEntropyLoss()
 
 for epoch in range(74):
@@ -93,7 +97,7 @@ for epoch in range(74):
     model.train()
 
     for images, lables in train_dataloader:
-        images, labels = images.to(device), labels.to(device)
+        images, lables = images.to(device), lables.to(device)
         
         optimizer.zero_grad()
         output = model(images)
@@ -106,7 +110,7 @@ for epoch in range(74):
 
         y_pred = torch.argmax(output, dim=1)
         correct += (y_pred == lables).sum().item()
-        total += labels.size()
+        total += lables.size(0)
 
     acc = correct/total
     print(f"Epoch {epoch+1}, Loss={total_loss/len(train_dataloader):.4f}, Acc={acc:.4f}")
