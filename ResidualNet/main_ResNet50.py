0228ac310c908427e2c22c74fccf8cd488945216
RudrapratapSinghTomer <rudrawork01@gmail.com>
2026-02-18 09:11:24 +0530

    Commit Working ResNet50

--- ResidualNet/main_ResNet50.py
+++ ResidualNet/main_ResNet50.py
@@ -12,18 +12,21 @@ class BottleNeck(nn.Module):
         self.expansion = expansion
         self.downsample = downsample
 
-        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, padding=1)
+        self.conv1 = nn.Conv2d(in_channel, out_channel,
+                       kernel_size=1, stride=1, padding=0, bias=False)
         self.bnorm1 = nn.BatchNorm2d(out_channel)
         self.relu1 = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=stride, padding=1)
+        self.conv2 = nn.Conv2d(out_channel, out_channel,
+                       kernel_size=3, stride=stride,
+                       padding=1, bias=False)
         self.bnorm2 = nn.BatchNorm2d(out_channel)
         self.relu2 = nn.ReLU(inplace=True)
 
-        out_channel = in_channel * expansion
+        final_out_channels = out_channel * expansion
 
-        self.conv3 = nn.Conv2d(in_channel, out_channel*self.expansion, kernel_size=1, stride=stride, padding=1)
-        self.bnorm3 = nn.BatchNorm2d(out_channel)
+        self.conv3 = nn.Conv2d(out_channel, out_channel * expansion, kernel_size=1, bias=False)
+        self.bnorm3 = nn.BatchNorm2d(out_channel * expansion)
         self.relu3 = nn.ReLU(inplace=True)
 
 
@@ -32,19 +35,19 @@ class BottleNeck(nn.Module):
 
         x = self.relu1(self.bnorm1(self.conv1(x)))
         x = self.relu2(self.bnorm2(self.conv2(x)))
-        x = self.relu3(self.bnorm3(self.conv2(x)))
+        x = self.relu3(self.bnorm3(self.conv3(x)))
 
         if self.downsample is not None:
-            identity = self.downsample(x)
+            identity = self.downsample(identity)
 
         x += identity
-        x = nn.ReLU(x)
+        x = torch.relu(x)
 
         return x
 
 class ResNet_50(nn.Module):
-    def __init__(self, img_channel, num_layer = 50, num_channel = 1000):
-        super(self, ResNet_50).__init__()
+    def __init__(self, img_channel, num_layer = 50, num_channel = 10):
+        super(ResNet_50, self).__init__()
         if num_layer==50:
             layers = [3, 4, 6, 3]
             self.expansion = 4
@@ -58,24 +61,25 @@ class ResNet_50(nn.Module):
 
         self.conv1 = self.makelayer(BottleNeck, 64, layers[0])
         self.conv2 = self.makelayer(BottleNeck, 128, layers[1], stride=2)
-        self.conv3 = self.makelayer(BottleNeck, 128, layers[2], stride=2)
-        self.conv4 = self.makelayer(BottleNeck, 128, layers[3], stride=2)
+        self.conv3 = self.makelayer(BottleNeck, 256, layers[2], stride=2)
+        self.conv4 = self.makelayer(BottleNeck, 512, layers[3], stride=2)
 
         self.avgpool0 = nn.AdaptiveAvgPool2d((1,1))
         self.fc0 = nn.Linear(512*self.expansion, num_channel)
 
     def makelayer(self, block: type[BottleNeck], out_channels, blocks, stride=2):
-        if stride != 1:
-            self.downsample = nn.Sequential(nn.Conv1d(in_channels=self.in_channnel, out_channels=out_channels, kernel_size=1, stride=stride),
+        downsample = None
+        if stride != 1 or self.in_channnel != out_channels * self.expansion:
+            downsample = nn.Sequential(nn.Conv2d(in_channels=self.in_channnel, out_channels=out_channels*self.expansion, kernel_size=1, stride=stride),
                                             nn.BatchNorm2d(out_channels*self.expansion),
                                             )
         
         layers = []
-        layers.append(BottleNeck(in_channel=self.in_channnel ,out_channel=out_channels, stride=stride, expansion=self.expansion, downsample=self.downsample))
+        layers.append(BottleNeck(in_channel=self.in_channnel ,out_channel=out_channels, stride=stride, expansion=self.expansion, downsample=downsample))
 
         self.in_channnel = out_channels*self.expansion
 
-        for i in range(1, blocks):
+        for _ in range(1, blocks):
             layers.append(BottleNeck(self.in_channnel, out_channel=out_channels, expansion=self.expansion))
 
         return nn.Sequential(*layers)
@@ -87,7 +91,7 @@ class ResNet_50(nn.Module):
 
         x = self.avgpool0(x)
 
-        x = nn.Flatten(x, 1)
+        x = torch.flatten(x, 1)
 
         x = self.fc0(x)
 
@@ -96,7 +100,7 @@ class ResNet_50(nn.Module):
 transform = transforms.Compose([transforms.Resize((256,256)), 
                                     transforms.RandomHorizontalFlip(), 
                                         transforms.ToTensor(), 
-                                        transforms.Normalize((0.5,) (0.5,))])
+                                        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
 
 train_dataset = datasets.CIFAR10(root='./path', train=True, transform=transform, download=True)
 test_dataset = datasets.CIFAR10(root='./path', train=False, transform=transform, download=True)
@@ -104,22 +108,22 @@ test_dataset = datasets.CIFAR10(root='./path', train=False, transform=transform,
 train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=256, shuffle=False)
 
-device = torch.device('cuda' if torch.cuda.is_available else 'cup')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = ResNet_50().to(device)
+model = ResNet_50(img_channel=3).to(device)
 
 optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
 loss_fn = nn.CrossEntropyLoss()
 
-for epoch in range():
+for epoch in range(5):
     model.train()
     correct = 0
     total = 0
     total_loss = 0
     for images, labels in train_dataloader:
-        images, lables = images.to(device), labels.to(device)
+        images, labels = images.to(device), labels.to(device)
 
-        optimizer.zero_grad
+        optimizer.zero_grad()
 
         output = model(images)
 
@@ -130,9 +134,9 @@ for epoch in range():
 
         total_loss += loss.item()
 
-        total += len(lables)
+        total += len(labels)
 
-        y_pred = torch.softmax(output)
-        y_pred = torch.argmax(y_pred)
+        y_pred = torch.softmax(output, dim=1)
+        y_pred = torch.argmax(y_pred, dim=1)
 
         correct += (y_pred == labels).sum().item()
