2ba079a00b98f63d6756ea7a9dbe32afeb81d3db
RudrapratapSinghTomer <rudrawork01@gmail.com>
2026-02-19 12:41:42 +0530

    Commit updated MobileNetv2

--- MobileNet/main_MobileNetv2.py
+++ MobileNet/main_MobileNetv2.py
@@ -5,61 +5,85 @@ import torch.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channel, out_channels, stride):
-        super(DepthwiseSeparableConv, self).__init__()
-        self.depthwise = nn.Sequential(nn.Conv2d(in_channel, in_channel, kernel_size=3, stride=stride, padding=1, groups=in_channel),
-                                       nn.BatchNorm2d(in_channel),
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, first=False):
+        super().__init__()
+        if first:
+            in_channels=3, out_channels=32, kernel_size=3, stride=2, padding=1, bias=False
+
+        else:
+            in_channels=320, out_channels=1280, kernel_size=1, stride=1, padding=0, bias=False
+
+        self.conv0 = nn.Sequential(nn.Conv2d(in_channels=in_channels, 
+                                                out_channels=out_channels, 
+                                                    kernel_size=kernel_size, 
+                                                        stride=stride, 
+                                                            padding=padding),
+                                   nn.BatchNorm2d(num_features=out_channels),
+                                   nn.ReLU6(inplace=True),
+                                   )
+        
+    def forward(self, x):
+        x = self.conv0(x)
+        return x
+
+class InvResidualS1(nn.Module):
+    def __init__(self, in_channels, out_channels, t):
+        super().__init__()
+        self.pointwise1 = nn.Sequential(nn.Conv1d(in_channels, out_channels=in_channels*t, kernel_size=1, stride=1, padding=0, bias=False),
+                                       nn.BatchNorm2d(in_channels*t),
                                             nn.ReLU6(inplace=True),
                                             )
         
-        self.pointwise = nn.Sequential(nn.Conv1d(in_channel, out_channels=out_channels, kernel_size=1, stride=stride, padding=1, groups=in_channel),
-                                       nn.BatchNorm2d(in_channel),
+        self.depthwise1 = nn.Sequential(nn.Conv2d(in_channels*t, in_channels*t, kernel_size=3, stride=1, padding=1, groups=in_channels*t, bias=False),
+                                       nn.BatchNorm2d(in_channels),
+                                            nn.ReLU6(inplace=True),
+                                            )
+
+        self.pointwise2 = nn.Sequential(nn.Conv1d(in_channels*t, out_channels=out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+                                       nn.BatchNorm2d(in_channels*t),
+                                            nn.ReLU6(inplace=True),
+                                            )
+
+    def forward(self, x):
+        identity = x
+
+        x = self.pointwise1(x)
+
+        x = self.depthwise1(x)
+
+        x = self.pointwise2(x)
+
+        x += identity
+
+        return x
+    
+class InvResidualS2(nn.Module):
+    def __init__(self, in_channels, out_channels, t):
+        super().__init__()
+        self.pointwise1 = nn.Sequential(nn.Conv1d(in_channels=in_channels, out_channels=in_channels*t, kernel_size=1, stride=1, padding=0, bias=False),
+                                       nn.BatchNorm2d(in_channels*t),
+                                            nn.ReLU6(inplace=True),
+                                            )
+        
+        self.depthwise1 = nn.Sequential(nn.Conv2d(in_channels*t, in_channels*t, kernel_size=3, stride=1, padding=1, groups=in_channels*t, bias=False),
+                                       nn.BatchNorm2d(in_channels),
+                                            nn.ReLU6(inplace=True),
+                                            )
+
+        self.pointwise2 = nn.Sequential(nn.Conv1d(in_channels*t, out_channels=out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+                                       nn.BatchNorm2d(in_channels*t),
                                             nn.ReLU6(inplace=True),
                                             )
         
     def forward(self, x):
-        x = self.depthwise(x)
+        x = self.pointwise1(x)
 
-        x = self.pointwise(x)
+        x = self.depthwise1(x)
 
-        return x
-    
-
-class BottleNeck(nn.Module):
-    def __init__(self, in_channels, out_channels, expansion):
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.expancion = expansion
-
-        self.conv0 = nn.Conv2d()
-
-        if self.stride == 2:
-            self.NetForStrideOne = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels*expansion, kernel_size=(1,1), bias=False), 
-                                                DepthwiseSeparableConv(in_channels*expansion)
-                                                )
-            
-            self.NetForStrideTwo = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels*expansion, kernel_size=(1,1), bias=False), 
-                                                 DepthwiseSeparableConv(in_channels*expansion, stride=(2,2))
-                                                )
-
-    def forward(self, input_image):
-        if self.expansion == 1:
-            x = self.depth(input_image)
-            x = self.pw(x)
-        else:
-            x = self.conv1(input_image)
-            x = self.dw(x)
-            x = self.pw(x)
-
-        # If input channel and output channel are same, then perform add
-        # residual part
-        if self.in_fts == self.out_fts:
-            x = input_image + x          
+        x = self.pointwise2(x)
 
         return x
-    
 
 class MobileNetv2_(nn.Module):
     def __init__(self, ):
